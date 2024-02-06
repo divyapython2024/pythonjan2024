9f236ea415019af034c75e79b8726c9cb5d06921
divyapython2024 <159038794+divyapython2024@users.noreply.github.com>
2024-02-06 03:06:36 +0000

    added savings bank pgm

---
+++ savings_banl.py
@@ -0,0 +1,8 @@
+#
+balance = 0
+min_deposit_balance = 1500
+salary_credited = 3300
+online_transaction = 33.33
+house_rent = 1500
+remaining_balance = balance + min_deposit_balance + salary_credited - online_transaction - house_rent
+print(f"Total balance remaining is {remaining_balance}")
